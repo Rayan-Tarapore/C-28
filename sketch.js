372cefccfae04820abbea302557f76f2d1359dd1
Rayan-Tarapore <ratitarapore@gmail.com>
2020-11-11 19:46:40 +0530

    I added the sling shot

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var bird, slingShot;
-
+var point 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -36,10 +37,10 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+
+    slingShot = new Sling(bird.body,{x:200,y:100});
 }
 
 function draw(){
@@ -63,6 +64,12 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    
+    slingShot.display();    
+}
+function mouseDragged(){
+    Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+    slingShot.fly();
 }
